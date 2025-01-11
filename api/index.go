8f64df71c8ea814bf668c3d3370d24ede8d4521d
kei19 <kitoyu22@gmail.com>
2025-01-11 22:22:09 +0700

    feat: initial add vercel.json and index.go

---
+++ api/index.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"../backend/src"
+)
+
+func main()  {
+	log.Println("...............API GYM...............")
+	src.Run()
+	log.Println(".....................................")
+}
+
